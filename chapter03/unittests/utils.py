e1378c507b76bb3d27bef5867f83e9352017e9fd
Snehal Tambe <packtsnehalt@github.com>
2020-03-05 07:38:44 -0800

    update

--- chapter03/unittests/utils.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-def prepare_data():
-    data = pd.read_csv(
-        "https://raw.githubusercontent.com/PacktWorkshops/Introduction-to-Data-Analysis/master/chapter03/data/bank-additional/bank-additional-full.csv",
-        sep=";")
-
-    return data
