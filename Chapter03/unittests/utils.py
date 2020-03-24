6717f2f6e19fab9871a8865d4c0a88bf1019512e
Snehal Tambe <45845632+packtsnehalt@users.noreply.github.com>
2020-03-23 23:57:41 -0700

    Update utils.py

--- Chapter03/unittests/utils.py
+++ Chapter03/unittests/utils.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def prepare_data():
     data = pd.read_csv(
-        "https://raw.githubusercontent.com/PacktWorkshops/Introduction-to-Data-Analysis/master/chapter03/data/bank-additional/bank-additional-full.csv",
+        "https://raw.githubusercontent.com/PacktWorkshops/The-Data-Analysis-Workshop/master/Chapter03/data/bank-additional/bank-additional-full.csv",
         sep=";")
 
     return data
